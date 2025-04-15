015b7fa9b4f618a656a68055cd05fb858af584d3
Prabjot Singh Gahir <prybinfo@gmail.com>
2025-04-15 16:44:48 +0530

    Changes in github workflow

--- upload.sh
+++ upload.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #source ./. env
 
-curdir = $(pwd)
-echo "${curdir}"
-echo "Running script"
+echo "Running script to zip and upload lambda"
+for f in lambdas/*;do
+  echo "${f}"
+  zipfile = $(echo ${f} | cut -f 1 -d '.').zip
+  zip -j "${zipfile}" "${f}"
