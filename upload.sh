add6e3edf0014ed97e8038dab2d9428d2cfecb0c
Prabjot Singh Gahir <prybinfo@gmail.com>
2025-04-15 17:02:05 +0530

    Changes in github workflow

--- upload.sh
+++ upload.sh
@@ -6,6 +6,7 @@ echo "$1"
 echo "Running script to zip and upload lambda"
 for f in lambdas/*;do
   echo "${f}"
-  zipfile = $(echo ${f} | cut -f 1 -d '.').zip
-  zip -j "${zipfile}" "${f}"
+  #zipfile = $(echo ${f} | cut -f 1 -d '.').zip
+  zip -j "$(echo ${f} | cut -f 1 -d '.').zip" "${f}"
+  echo "$(cut -f 1 -d '.').zip"
 done
